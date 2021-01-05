b9f2b0c1fddba68bf9dfcc409e00aa9b5d376b46
GunjanBhat1a <76939553+GunjanBhat1a@users.noreply.github.com>
2021-01-05 20:58:54 +0530

    Class for finding prime factors

---
+++ src/main/java/simplest/possible/implementation/PrimeFactors.java
@@ -0,0 +1,12 @@
+package simplest.possible.implementation;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class PrimeFactors {
+    private List<Integer> factorsList = new ArrayList<Integer>();
+
+    public List<Integer> of(int number) {
+        return factorsList;
+    }
+}
